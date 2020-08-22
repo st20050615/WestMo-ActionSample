4c15021c461b90194bb31b56ec20a9377e8d5b90
runmon <st20050615@gmail.com>
2020-08-22 16:48:31 +0900

    サンプル５－３　キャラクターの移動

---
+++ Assets/Scripts/NejikoController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class NejikoController : MonoBehaviour
+{
+    CharacterController controller;
+    Animator animator;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    public float gravity;
+    public float speedZ;
+    public float speedJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 必要なコンポーネントを自動取得
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 地上にいる場合のみ操作を行う
+        if (controller.isGrounded)
+        {
+            // Inputを検知して前にすすめる
+            if (Input.GetAxis("Vertical") > 0.0f)
+            {
+                moveDirection.z = Input.GetAxis("Vertical") * speedZ;
+            } else {
+                moveDirection.z = 0;
+            }
+        }
+
+        // 方向転換
+        transform.Rotate(0, Input.GetAxis("Horizontal") * 3, 0);
+
+        // ジャンプ
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = speedJump;
+            animator.SetTrigger("jump");
+        }
+
+        // 重力分の力をフレーム追加
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // 移動実行
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);
+        controller.Move(globalDirection * Time.deltaTime);
+
+        // 移動後接地してたらY方向の速度はリセットする
+        if (controller.isGrounded) moveDirection.y = 0;
+
+        // 速度が0以上なら走っているフラグをtrueにする
+        animator.SetBool("run", moveDirection.z > 0.0f);
+    }
+}
