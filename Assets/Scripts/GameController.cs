9ce8caceab76de9ff0b17d27b7a142649cce214f
runmon <st20050615@gmail.com>
2020-08-23 17:00:43 +0900

    サンプル５－７　GUI表示

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public Text scoreLabel;
+    public LifePanel lifePanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // スコアラベルを更新
+        int score = CalcScore();
+        scoreLabel.text = "Score : " + score + "m";
+
+        // ライフパネルを更新
+        lifePanel.UpdateLife(nejiko.Life());
+    }
+
+    int CalcScore()
+    {
+        // ねじ子の走行距離をスコアとする
+        return (int)nejiko.transform.position.z;
+    }
+}
